263fb6f746a72c8627ed692792d47c747c0e8960
Morten Johansen <mj@emsg.net>
2026-07-06 17:24:03 +0200

    fix: add AGPL-3.0 license headers to Task 1 files

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2026 Bifröst Contributors
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the LICENSE file for details.
+ */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
